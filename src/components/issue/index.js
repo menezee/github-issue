3f4d19d30cff2aae9982f033deaa163c952c8a49
Erich Menezes <erich.menezes1@gmail.com>
2019-07-23 23:01:38 -0700

    :lipstick: adds spacing for issue info container

--- src/components/issue/index.js
+++ src/components/issue/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import {ButtonPrimary, StateLabel, Label} from '@primer/components';
 import styles from './issue.module.scss';
 
 export const Issue = ({title, id, state, onClick}) => (
-    <div className="Box-row d-flex flex-items-center">
-      <div className="flex-auto">
+    <div className='Box-row d-flex flex-items-center'>
+      <div className={classnames(styles['issue__info-container'])}>
 
         {/* TITLE */}
         <Label mr={2} className='Label--gray'>{id}</Label>
         <strong>- {title}</strong>
-        <div className="text-small text-gray-light d-flex flex-items-center">
+        <div className='text-small text-gray-light d-flex flex-items-center'>
 
           {/* STATE */}
           <StateLabel status={state === 'open' ? 'issueOpened' : 'issueClosed'} my={2} mr={1} />
